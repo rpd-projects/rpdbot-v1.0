7eb51d8f52f9b3582e5b0b3a569e87dae511ee1a
TobyGaming74 <tobyprogamerz@gmail.com>
2017-12-26 22:20:13 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ require_once('./line_class.php');
 require_once('./unirest-php-master/src/Unirest.php');
 
 $channelAccessToken = 'vd1bR2yHFTI4P72I1dttBff7KDCVZBVIkRzYo9ump0w+isoeA47q+m1Jea6f8NeZu5/AiOWSXSW6zyYTuzf7XDXj8/gyblUF19yrzGr9RJpBq0fEsJEYIn53WzQCFsywFH5gnIChiLYG9JKy/hSN6wdB04t89/1O/w1cDnyilFU='; //sesuaikan 
-$channelSecret = 'e86c79bc189716a141bb0ecd71418e58';//sesuaikan
+$channelSecret = 'b151ace4cb6df5dc6d3e4c95bdf47f92';//sesuaikan
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
@@ -36,50 +36,15 @@ if (count($pesan_datang) > 2) {
     }
 }
 
-#-------------------------[Function]-------------------------#
-function cuaca($keyword) {
-    $uri = "http://api.openweathermap.org/data/2.5/weather?q=" . $keyword . ",ID&units=metric&appid=e172c2f3a3c620591582ab5242e0e6c4";
-
-    $response = Unirest\Request::get("$uri");
-
-    $json = json_decode($response->raw_body, true);
-    $result = "======[InfoCuaca]======";
-	$result .= $json['name'];
-	$result .= " Dan Sekitarnya";
-	$result .= "\n\nCuaca : ";
-	$result .= $json['weather']['0']['main'];
-	$result .= "\nDeskripsi : ";
-	$result .= $json['weather']['0']['description'];
-    return $result;
-}
-function ngegrab($url){
-    ini_set("user_agent","Opera/9.80 (J2ME/MIDP; Opera Mini/4.2 19.42.55/19.892; U; en) Presto/2.5.25");
-    $grab = @fopen($url, 'r');
-    $contents = "";
-    if ($grab) {
-        while (!feof($grab)) {
-        $contents.= fread($grab, 8192);
-    }
-    fclose($grab);
-}
-return $contents;
-}
-#-------------------------[Function]-------------------------#
-
-# require_once('./src/function/search-1.php');
-# require_once('./src/function/download.php');
-# require_once('./src/function/random.php');
-# require_once('./src/function/search-2.php');
-# require_once('./src/function/hard.php');
-
-#-------------------------[Function]-------------------------#
+#-------------------------[Function]-------------------------#"
 function shalat($keyword) {
     $uri = "https://time.siswadi.com/pray/" . $keyword;
 
     $response = Unirest\Request::get("$uri");
 
     $json = json_decode($response->raw_body, true);
-    $result = "Jadwal Shalat Sekitar ";
+    $result = "====[JadwalShalat]====";
+    $result .= "\nLokasi : ";
 	$result .= $json['location']['address'];
 	$result .= "\nTanggal : ";
 	$result .= $json['time']['date'];
@@ -93,6 +58,103 @@ function shalat($keyword) {
 	$result .= $json['data']['Maghrib'];
 	$result .= "\nIsya : ";
 	$result .= $json['data']['Isha'];
+	$result .= "\n\nPencarian : Google";
+	$result .= "\n====[JadwalShalat]====";
+    return $result;
+}
+#-------------------------[Function]-------------------------#
+
+# require_once('./src/function/search-1.php');
+# require_once('./src/function/download.php');
+# require_once('./src/function/random.php');
+# require_once('./src/function/search-2.php');
+# require_once('./src/function/hard.php');
+
+#-------------------------[Function]-------------------------#"
+function kalender($keyword) {
+    $uri = "https://time.siswadi.com/pray/" . $keyword;
+
+    $response = Unirest\Request::get("$uri");
+
+    $json = json_decode($response->raw_body, true);
+    $result = "====[Kalender]====";
+	$result .= "\nTanggal : ";
+	$result .= $json['time']['date'];
+	$result .= "\nWaktu : ";
+	$result .= $json['time'];
+	$result .= "\n\nPencarian : Google";
+	$result .= "\n====[Kalender]====";
+    return $result;
+}
+#-------------------------[Function]-------------------------#
+
+# require_once('./src/function/search-1.php');
+# require_once('./src/function/download.php');
+# require_once('./src/function/random.php');
+# require_once('./src/function/search-2.php');
+# require_once('./src/function/hard.php');
+
+#-------------------------[Function]-------------------------#
+function cuaca($keyword) {
+    $uri = "http://api.openweathermap.org/data/2.5/weather?q=" . $keyword . ",ID&units=metric&appid=e172c2f3a3c620591582ab5242e0e6c4";
+    $response = Unirest\Request::get("$uri");
+
+    $json = json_decode($response->raw_body, true);
+    $result = "====[InfoCuaca]====";
+    $result .= "\nKota : ";
+	$result .= $json['name'];
+	$result .= "\nCuaca : ";
+	$result .= $json['weather']['0']['main'];
+	$result .= "\nDeskripsi : ";
+	$result .= $json['weather']['0']['description'];
+	$result .= "\n\nPencariaan : Google";
+	$result .= "\n====[InfoCuaca]====";
+    return $result;
+}
+#-------------------------[Function]-------------------------#
+
+# require_once('./src/function/search-1.php');
+# require_once('./src/function/download.php');
+# require_once('./src/function/random.php');
+# require_once('./src/function/search-2.php');
+# require_once('./src/function/hard.php');
+
+#-------------------------[Function]-------------------------#
+function lokasi($keyword) {
+    $uri = "https://time.siswadi.com/pray/" . $keyword;
+
+    $response = Unirest\Request::get("$uri");
+
+    $json = json_decode($response->raw_body, true);
+    $result = "====[Lokasi]====";
+    $result .= "\nLokasi : ";
+	$result .= $json['location']['address'];
+	$result .= "\n\nPencarian : Google";
+	$result .= "\n====[Lokasi]====";
+    return $result;
+}
+#-------------------------[Function]-------------------------#
+
+# require_once('./src/function/search-1.php');
+# require_once('./src/function/download.php');
+# require_once('./src/function/random.php');
+# require_once('./src/function/search-2.php');
+# require_once('./src/function/hard.php');
+
+#-------------------------[Function]-------------------------#
+function lirik($keyword) {
+    $uri = "http://ide.fdlrcn.com/workspace/yumi-apis/joox?" . $keyword;
+
+    $response = Unirest\Request::get("$uri");
+
+    $json = json_decode($response->raw_body, true);
+    $result = "====[Lirik]====";
+    $result .= "\nJudul : ";
+	$result .= $json['data']['song[0]'];
+	$result .= "\nLirik : ";
+	$result .= $json['data']['song[5]'];
+	$result .= "\n\nPencarian : Google";
+	$result .= "\n====[Lirik]====";
     return $result;
 }
 #-------------------------[Function]-------------------------#
@@ -105,7 +167,7 @@ function shalat($keyword) {
 
 //show menu, saat join dan command /menu
 if ($type == 'join' || $command == '/menu') {
-    $text = "Terima kasih telah invite aku ke group ya kak..";
+    $text = "Terima kasih telah invite aku ke group ya kak..\nKetik 'Help' untuk menggunakan Bot";
     $balas = array(
         'replyToken' => $replyToken,
         'messages' => array(
@@ -116,26 +178,6 @@ if ($type == 'join' || $command == '/menu') {
         )
     );
 }
-if($message['type']=='text') {
-        if ($command == '/lokasi') {
-        $data=json_decode(ngegrab('https://maps.googleapis.com/maps/api/geocode/json?address='.$loc.'&key=AIzaSyAzuwWTOehoOope1LMsFk9Pf6bc49I9sds'),true);
-        $address=$data[results][0][formatted_address];
-        $lat  = $data[results][0][geometry][location][lat];
-        $lng  = $data[results][0][geometry][location][lng];
-        $result = cuaca($options);
-        $balas = array(
-            'replyToken' => $replyToken,
-            'messages' => array(
-                array(
-                    'type' => 'location',
-                    'title' => 'Lokasi',
-                    'address' => $address,
-                    'latitude' => $lat,
-                    'longitude' => $lng
-                )
-            )
-        );
-    }
 
 //pesan bergambar
 if($message['type']=='text') {
@@ -152,11 +194,57 @@ if($message['type']=='text') {
             )
         );
     }
+}
+if($message['type']=='text') {
+	    if ($command == '/lokasi') {
+
+        $result = lokasi($options);
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => $result
+                )
+            )
+        );
+    }
+}
+if($message['type']=='text') {
+	    if ($command == '/kalender') {
+
+        $result = kalender($options);
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => $result
+                )
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($commmnd == '/lirik') {
+
+        $result = lirik($options);
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => $result
+                )
+            )
+        );
+    }
+}
 
 //pesan bergambar
 if($message['type']=='text') {
-	    if ($command == '/shalat') {
-
+	    if ($commmnd == '/shalat') {
         $result = shalat($options);
         $balas = array(
             'replyToken' => $replyToken,
@@ -192,5 +280,4 @@ if (isset($balas)) {
 
     $client->replyMessage($balas);
 }
-
 ?>
